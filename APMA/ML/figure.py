7d73a8e1bad28a732a2235ae3f1660b5fe24536c
LoveUCB <140378072+LoveUCB@users.noreply.github.com>
2024-02-12 11:52:58 +0800

    Delete APMA directory

--- APMA/ML/figure.py
+++
@@ -1,5 +0,0 @@
-from rpy2.robjects.packages import importr
-import rpy2.robjects as robjects
-
-# 绘制
-
--- APMA/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="APMA",
-    version = "1.0",
-    description = "APMA",
-    author = "Wang Jingran",
-    py_modules = ["APMA.Feature","APMA.ML"]
-)
